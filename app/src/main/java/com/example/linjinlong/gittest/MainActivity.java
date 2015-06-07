2d1dd0b2299b73f8ab671d470e5a42a972e85d64
linjlflow <linjlflow@gmail.com>
2015-06-07 10:52:41 +0800

    修改了内容

--- app/src/main/java/com/example/linjinlong/gittest/MainActivity.java
+++ app/src/main/java/com/example/linjinlong/gittest/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //才13k的工资
         setContentView(R.layout.activity_main);
     }
 
